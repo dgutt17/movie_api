10a8e4779cdc1d8362f6d3de94e1856fc55c2f1d
Dan Gutt <dgutt10@gmail.com>
2021-11-04 21:55:10 -0500

    Pulling down the gzip file from imdb in an automated way

---
+++ app/services/imdb/importer.rb
@@ -0,0 +1,32 @@
+require 'open-uri'
+
+module Imdb
+  class Importer
+    attr_reader :ratings_file, :movies_file
+
+    def initialize
+      @ratings_file = gzip_reader(ENV['RATINGS_FILE_URL'])
+      @movies_file = gzip_reader(ENV['MOVIES_FILE_URL'])
+    end
+
+    def run
+      puts "Printing Ratings file"
+      sleep(5)
+      ratings_file.each_line do |line|
+        puts line
+      end
+
+      puts "Printing Movies file"
+      sleep(5)
+      movies_file.each_line do |line|
+        puts line
+      end
+    end
+
+    private
+
+    def gzip_reader(file_url)
+      Zlib::GzipReader.new(URI.open(file_url))
+    end
+  end
+end
