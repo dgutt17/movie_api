ffe757dd8163c5c07fce1fa46e89d3b9c65dec02
Dan Gutt <dgutt10@gmail.com>
2021-11-15 22:38:06 -0600

    Storing ratings in a hash

--- app/services/imdb/importer.rb
+++ app/services/imdb/importer.rb
@@ -2,20 +2,27 @@ require 'open-uri'
 
 module Imdb
   class Importer
-    attr_reader :ratings_file, :movies_file
+    attr_reader :ratings_file, :movies_file, :ratings
 
     def initialize
       @ratings_file = gzip_reader(ENV['RATINGS_FILE_URL'])
       @movies_file = gzip_reader(ENV['MOVIES_FILE_URL'])
+      @ratings = {}
     end
 
     def run
       puts "Printing Ratings file"
       sleep(5)
-      ratings_file.each_line do |line|
-        puts line
+      ratings_file.each_line do |row|
+        parsed_row = row.split("\t")
+        if parsed_row.last.to_i >= 500
+          puts "row: #{parsed_row}"
+          parsed_row[parsed_row.length - 1] = parsed_row.last.split("\n").first
+          ratings[parsed_row.first] = parsed_row.slice(1, parsed_row.length)
+        end
       end
 
+      binding.pry
       puts "Printing Movies file"
       sleep(5)
       movies_file.each_line do |line|
